41338415d685ad4283f801e1424ec8f0b8d61f31
Philipp Baumeister <philipp_baumeister@gmx.de>
2019-07-18 15:03:45 +0200

    Initial commit

---
+++ mdn/__init__.py
@@ -0,0 +1,101 @@
+"""
+A Mixture Density Layer for Keras
+cpmpercussion: Charles Martin (University of Oslo) 2018
+https://github.com/cpmpercussion/keras-mdn-layer
+
+Hat tip to [Omimo's Keras MDN layer](https://github.com/omimo/Keras-MDN) for a starting point for this code.
+
+modified by Philipp Baumeister (TU Berlin) 2019
+https://github.com/philippbaumeister/MDN_exoplanets
+"""
+import keras
+from keras import backend as K
+from keras.layers import Dense
+from keras.engine.topology import Layer
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+
+
+def elu_plus_one_plus_epsilon(x):
+    """ELU activation with a very small addition to help prevent NaN in loss."""
+    return K.elu(x) + 1 + K.epsilon()
+
+
+class MDN(Layer):
+    """A Mixture Density Network Layer for Keras.
+    This layer has a few tricks to avoid NaNs in the loss function when training:
+        - Activation for variances is ELU + 1 + 1e-8 (to avoid very small values)
+        - Mixture weights (pi) are trained in as logits, not in the softmax space.
+
+    A loss function needs to be constructed with the same output dimension and number of mixtures.
+    A sampling function is also provided to sample from distribution parametrised by the MDN outputs.
+    """
+
+    def __init__(self, output_dimension, num_mixtures, **kwargs):
+        self.output_dim = output_dimension
+        self.num_mix = num_mixtures
+        with tf.name_scope('MDN'):
+            self.mdn_mus = Dense(self.num_mix * self.output_dim, activation=elu_plus_one_plus_epsilon, name='mdn_mus')  # mix*output vals, exp activation
+            self.mdn_sigmas = Dense(self.num_mix * self.output_dim, activation=elu_plus_one_plus_epsilon, name='mdn_sigmas')  # mix*output vals exp activation
+            self.mdn_pi = Dense(self.num_mix, name='mdn_pi', activation='softmax')  # mix vals, softmax activation
+        super(MDN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.mdn_mus.build(input_shape)
+        self.mdn_sigmas.build(input_shape)
+        self.mdn_pi.build(input_shape)
+        self.trainable_weights = self.mdn_mus.trainable_weights + self.mdn_sigmas.trainable_weights + self.mdn_pi.trainable_weights
+        self.non_trainable_weights = self.mdn_mus.non_trainable_weights + self.mdn_sigmas.non_trainable_weights + self.mdn_pi.non_trainable_weights
+        super(MDN, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        with tf.name_scope('MDN'):
+            mdn_out = keras.layers.concatenate([self.mdn_mus(x),
+                                                self.mdn_sigmas(x),
+                                                self.mdn_pi(x)],
+                                               name='mdn_outputs')
+        return mdn_out
+
+    def compute_output_shape(self, input_shape):
+        """Returns output shape, showing the number of mixture parameters."""
+        return (input_shape[0], (2 * self.output_dim * self.num_mix) + self.num_mix)
+
+    def get_config(self):
+        config = {
+            "output_dimension": self.output_dim,
+            "num_mixtures": self.num_mix
+        }
+        base_config = super(MDN, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+def get_mixture_loss_func(output_dim, num_mixes):
+    """Construct a loss functions for the MDN layer parametrised by number of mixtures."""
+    # Construct a loss function with the right number of mixtures and outputs
+    def loss_func(y_true, y_pred):
+        # Reshape inputs in case this is used in a TimeDistribued layer
+        y_pred = tf.reshape(y_pred, [-1, (2 * num_mixes * output_dim) + num_mixes], name='reshape_ypreds')
+        y_true = tf.reshape(y_true, [-1, output_dim], name='reshape_ytrue')
+        # Split the inputs into paramaters
+        out_mu, out_sigma, out_pi = tf.split(y_pred, num_or_size_splits=[num_mixes * output_dim,
+                                                                         num_mixes * output_dim,
+                                                                         num_mixes],
+                                             axis=-1, name='mdn_coef_split')
+        # Construct the mixture models
+        cat = tfd.Categorical(probs=out_pi)
+        component_splits = [output_dim] * num_mixes
+        mus = tf.split(out_mu, num_or_size_splits=component_splits, axis=1)
+        sigs = tf.split(out_sigma, num_or_size_splits=component_splits, axis=1)
+        coll = [tfd.MultivariateNormalDiag(loc=loc, scale_diag=scale) for loc, scale
+                in zip(mus, sigs)]
+        mixture = tfd.Mixture(cat=cat, components=coll)
+        loss = mixture.log_prob(y_true)
+        loss = tf.negative(loss)
+        loss = tf.reduce_mean(loss)
+        return loss
+
+    # Actually return the loss_func
+    with tf.name_scope('MDN'):
+        return loss_func
